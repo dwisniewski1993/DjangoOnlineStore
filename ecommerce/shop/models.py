1af823d6573f2b21dbde9c6cc0b3dbe2b6b0c4e6
dwisniewski1993 <dwisniewski@st.amu.edu.pl>
2018-08-06 22:11:57 +0200

    Add urls, templates and images

--- ecommerce/shop/models.py
+++ ecommerce/shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -16,6 +17,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])
 
 
 class Product(models.Model):
@@ -36,3 +40,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
