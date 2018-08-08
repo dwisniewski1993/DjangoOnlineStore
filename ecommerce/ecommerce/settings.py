b6aa19181fba9a4f4112a208cd376d3b4f195cf6
dwisniewski1993 <dwisniewski@st.amu.edu.pl>
2018-08-08 18:35:48 +0200

    Add orders models and forms

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'cart.apps.CartConfig',
     'shop.apps.ShopConfig',
+    'orders.apps.OrdersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
