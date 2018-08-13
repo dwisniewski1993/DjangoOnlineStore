bd211b1c58356f01bd5cc734f1cdcc24174be538
dwisniewski1993 <dwisniewski1993@tlen.pl>
2018-08-13 17:45:24 +0200

    Change language to poliish

--- ecommerce/cart/forms.py
+++ ecommerce/cart/forms.py
@@ -4,5 +4,5 @@ PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 26)]
 
 
 class CartAddProductForm(forms.Form):
-    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
+    ile = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
     update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -12,7 +12,7 @@ def cart_add(request, product_id):
     form = CartAddProductForm(request.POST)
     if form.is_valid():
         cd = form.cleaned_data
-        cart.add(product=product, quantity=cd['quantity'], update_quantity=cd['update'])
+        cart.add(product=product, quantity=cd['ile'], update_quantity=cd['update'])
     return redirect('cart:cart_detail')
 
 
@@ -26,5 +26,5 @@ def cart_remove(request, product_id):
 def cart_detail(request):
     cart = Cart(request)
     for item in cart:
-        item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'], 'update': True})
+        item['update_quantity_form'] = CartAddProductForm(initial={'ile': item['quantity'], 'update': True})
     return render(request, 'cart/detail.html', {'cart': cart})
